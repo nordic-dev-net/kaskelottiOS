2992fae24fc9e37c33f8c8219ff4ecb1d9cf3d59
Julius Koskela <me@juliuskoskela.dev>
2023-10-25 21:23:30 +0300

    Implement kaskelotti-help command
    Signed-off-by: Julius Koskela <me@juliuskoskela.dev>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# kaskelottiOS
 KaskelottiOS
+
+This is a NixOS based system configuration intended for marine mammal research.
+
+Commands:
+
+- kaskelotti-help: Show system help.
+- kaskelotti-update: This command will update the system.
--- user/aliases.nix
+++ user/aliases.nix
@@ -1,5 +1,6 @@
 {
   # Nix commands
+  kaskelotti-help = "pushd ~/kaskelottiOS; cat README.md; popd";
   kaskelotti-update = "pushd ~/kaskelottiOS; git fetch; git pull; sudo nixos-rebuild --flake '.#kaskelotti' switch; popd";
 
   # Editor
