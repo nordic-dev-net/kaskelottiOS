013caf03b90cdb3554a36379f74dfd68b23b2937
Julius Koskela <me@juliuskoskela.dev>
2023-10-25 21:29:37 +0300

    Make help command silent
    Signed-off-by: Julius Koskela <me@juliuskoskela.dev>

--- user/aliases.nix
+++ user/aliases.nix
@@ -1,6 +1,6 @@
 {
   # Nix commands
-  kaskelotti-help = "pushd ~/kaskelottiOS > /dev/null; cat README.md; popd > /dev/null";
+  kaskelotti-help = "pushd -q ~/kaskelottiOS; cat README.md; popd -q";
   kaskelotti-update = "pushd ~/kaskelottiOS; git fetch; git pull; sudo nixos-rebuild --flake '.#kaskelotti' switch; popd";
 
   # Editor
