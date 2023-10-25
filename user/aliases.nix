aa4f3c2fbaf5e045dad1914223b64857015d3ab8
Julius Koskela <me@juliuskoskela.dev>
2023-10-25 21:18:29 +0300

    Remove unnecessary aliases
    Signed-off-by: Julius Koskela <me@juliuskoskela.dev>

--- user/aliases.nix
+++ user/aliases.nix
@@ -1,9 +1,4 @@
 {
-  # Keyboard layout
-  fi = "hyprctl keyword input:kb_layout fi";
-  us = "hyprctl keyword input:kb_layout us";
-  no = "hyprctl keyword input:kb_layout no";
-
   # Nix commands
   kaskelotti-update = "pushd ~/kaskelottiOS; git fetch; git pull; sudo nixos-rebuild --flake '.#kaskelotti' switch; popd";
 
